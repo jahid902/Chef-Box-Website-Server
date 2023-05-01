f9d9b6c7a02dcb1c4dba457621fedfc07c03b958
jahid902 <jahidjr902@gmail.com>
2023-05-01 22:52:50 +0600

    added chefs json file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000 ;
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('chef server running')
 })
 
 app.listen(port, () => {
