9594745f8c3149e6602cb43190d26c8449a2998c
jahid902 <jahidjr902@gmail.com>
2023-05-01 22:59:02 +0600

    created route and and loaded chef data in local host

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000 ;
+
+const chefData = require("./data/chefsData.json")
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('chef server running')
 })
+
+app.get('/chefs', (req,res) => {
+    res.send(chefData);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
